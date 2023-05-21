bf12b9f749958acee21185c04a233fb9f4b1914e
fedorovigor <fedorovigord@gmail.com>
2023-05-21 13:42:19 +0300

    added project list with button to create new project

---
+++ task_manager/src/app/model/status_project.ts
@@ -0,0 +1,4 @@
+export interface StatusProject {
+    id: number;
+    status: string;
+}
--- task_manager/src/app/project-list/project-list.component.ts
+++ task_manager/src/app/project-list/project-list.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../model/project';
+import { ProjectService } from '../project.service';
+import { Dialog } from '@angular/cdk/dialog';
+import { ProjectDialog } from './dialog/project_dialog';
 
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.css']
 })
-export class ProjectListComponent {
+export class ProjectListComponent implements OnInit {
 
+  projects !: Project[];
+  newProject: Project =  {
+    id: 0,
+    name: "",
+    description: "",
+    startData: new Date,
+    status: {
+      id: 0,
+      status: "NEW"
+    }
+  };
+
+  constructor(private projectSevice: ProjectService,
+    public dialog: Dialog) {}
+
+  ngOnInit(): void {
+    this.projectSevice.getProjects()
+      .subscribe(projects => this.projects = projects);
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open<Project>(ProjectDialog, {
+      width: '550px',
+      data: {animal: this.newProject},
+    });
+
+    dialogRef.closed.subscribe(result => {
+      console.log('The dialog was closed');
+
+      if (result !== undefined) {
+        this.newProject = result;
+        this.createProject(this.newProject);
+      }
+    });
+  }
+  
+
+  onProject(project: Project) {
+    console.log("click on project id " + project.id)
+  }
+
+  createProject(project: Project) {
+    this.projectSevice.createProject(project).subscribe();
+  }
 }
